b0d32370004163d28e37ed04bf8f010973a8cb5a
phoenix377 <phoenix.number377@gmail.com>
2022-02-28 12:52:25 -0800

    get campground detail with api

--- src/components/CampgroundOverview/index.js
+++ src/components/CampgroundOverview/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import searchResults from '../../data/searchResults';
-
+import React, { useEffect, useState } from 'react';
 import styles from './CampgroundOverview.module.scss';
+import { ItemDetail } from "../ItemDetail";
 
-const CampgroundOverview = ({ selectedCampgroundId }) => {
-  // TODO load the actual campgrond from https://thedyrt.com/api/v5/campgrounds/${selectedCampgroundId}
-  const campground = searchResults.find(
-    (result) => selectedCampgroundId === result.id
-  );
+const CampgroundOverview = (props) => {
+  const { selectedCampgroundId } = props;
+  const [campground, setCampground] = useState();
+
+  const getCompground = async () => {
+    try {
+      const res = await fetch(`https://thedyrt.com/api/v5/campgrounds/${selectedCampgroundId}`);
+      const data = await res.json();
+      setCampground(data.data.attributes)
+    } catch (error) {
+      console.log(error, '====> e <====');
+    }
+  }
+
+  useEffect(() => {
+    getCompground();
+  }, [selectedCampgroundId]);
 
   return (
     <div className={styles['overview']}>
       <div className={styles['overview__content']}>
-        <h2>{`Display Content About ${campground?.name}`}</h2>
+        {campground?.photoUrl &&
+          <img src={campground.photoUrl} alt={campground.name}/>
+        }
+        <h2>{campground?.name}</h2>
       </div>
     </div>
   );
